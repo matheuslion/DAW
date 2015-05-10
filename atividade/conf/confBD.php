5fa7e5276434940a3f0cf1403acbf94cf226f33b
matheuslion <matheusleaodemelo@gmail.com>
2015-05-10 18:48:41 -0300

    Banco de dados funcionando

--- atividade/conf/confBD.php
+++ atividade/conf/confBD.php
@@ -1,19 +1,22 @@
 <?php
  function conn_mysql(){
 
-   $servidor = 'localhost';
-   $porta = 3306;
-   $banco = "daw_yearbook";
-   $usuario = "root";
-   $senha = "";
-   
-      $conn = new PDO("mysql:host=$servidor;
-	                   port=$porta;
-					   dbname=$banco", 
-					   $usuario, 
-					   $senha,
-					   array(PDO::ATTR_PERSISTENT => true)
-					   );
-      return $conn;
+ 		try{
+			$host = 'br-cdbr-azure-south-a.cloudapp.net';
+			$usuario = 'b8aa3fe301542d';
+			$senha='7e119973';
+			$banco = 'atividaAXUwEFlHL';
+			$charset = "utf8"; 
+			$string_conexao = "mysql:host=$host;dbname=$banco;charset=$charset;";
+			
+			$conn = new PDO($string_conexao,$usuario,$senha);
+			
+			return $conn;
+		}
+		catch(PDOException $e){
+			echo 'Erro: '.$e->getMessage().'<br />';
+			die();
+		}
+		
    }
 ?>
